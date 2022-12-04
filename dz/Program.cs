3015fe23e9322eee3c79502caadc2b72a9553edb
Victoria <molcanovaviktoria660@gmail.com>
2022-12-04 22:37:25 +0700

    ex3

---
+++ dz/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Введите день недели ");
+int d = Convert.ToInt32(Console.ReadLine());
+if (d==6||d==7)
+{
+Console.Write("Выходной день");
+}
+else
+{
+     Console.Write("будни");
+}
